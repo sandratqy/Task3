39f9c47a1e1423cc87df3ab4e5a003cbb7b29c7a
Sandra Tan <161189K@mymail.nyp.edu.sg>
2018-02-02 12:14:06 +0800

    Task3 ver 3
    Signed-off-by: Sandra Tan <161189K@mymail.nyp.edu.sg>

--- src/test/java/RentalAnalysisTest.java
+++ src/test/java/RentalAnalysisTest.java
@@ -6,14 +6,14 @@ public class RentalAnalysisTest {
 
 	@Test
 	public void testAveMonthlyRentalDur() {
-		assertEquals("Fail1", RentalAnalysis.aveMonthlyRentalDur(), 28 , 0.0);
-		assertEquals("Fail2", RentalAnalysis.aveMonthlyRentalDur(), 26 , 0.0);
+		assertEquals("Fail1", 28, RentalAnalysis.aveMonthlyRentalDur(),  0.0);
+		assertEquals("Fail2", 26, RentalAnalysis.aveMonthlyRentalDur(), 0.0);
 	}
 	
 	@Test
 	public void testTotalRev() {
-		assertEquals("Fail1", RentalAnalysis.totalRev(),10497.5 , 0.0);
-		assertEquals("Fail2", RentalAnalysis.totalRev(),12160.0 , 0.0);
+		assertEquals("Fail1", 10497.5, RentalAnalysis.totalRev(), 0.0);
+		assertEquals("Fail2", 12160.0, RentalAnalysis.totalRev(), 0.0);
 	}
 
 }
